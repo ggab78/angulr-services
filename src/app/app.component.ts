a937a8694ef3c90d49d27f61e500fad420b40a11
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-02-02 16:10:07 +0100

    move service injection to the highest level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import {AccountsService} from './accounts.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [AccountsService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
@@ -18,11 +17,4 @@ export class AppComponent implements OnInit {
     this.accounts = this.accountsService.accounts;
   }
 
-  // onAccountAdded(newAccount: {name: string, status: string}) {
-  //   this.accounts.push(newAccount);
-  // }
-  //
-  // onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-  //   this.accounts[updateInfo.id].status = updateInfo.newStatus;
-  // }
 }
